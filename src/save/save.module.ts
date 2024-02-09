5114ae6ca8d14d545a51942744cd32f5e18bf785
Davi Monteiro <monteiro_davi@yahoo.com>
2024-02-09 14:50:05 -0300

    Adicionando tratamento de exceção no createSave

--- src/save/save.module.ts
+++ src/save/save.module.ts
@@ -3,9 +3,10 @@ import { SaveController } from './save.controller';
 import { SaveService } from './save.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SaveEntity } from './entities/save.entity';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SaveEntity])],
+  imports: [TypeOrmModule.forFeature([SaveEntity]), UserModule],
   controllers: [SaveController],
   providers: [SaveService],
 })
--- src/save/save.service.ts
+++ src/save/save.service.ts
@@ -3,18 +3,22 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { SaveEntity } from './entities/save.entity';
 import { CreateSaveDto } from './dtos/createSave.dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class SaveService {
   constructor(
     @InjectRepository(SaveEntity)
     private readonly saveRepository: Repository<SaveEntity>,
+    private readonly userService: UserService,
   ) {}
 
   async createSave(
     createSaveDto: CreateSaveDto,
     idUser: number,
   ): Promise<SaveEntity> {
+    await this.userService.getUserById(idUser);
+
     return this.saveRepository.save({
       ...createSaveDto,
       idUser,
