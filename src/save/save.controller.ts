f817257b4a5cee6e38aa36c5effa2609da1df9c1
Davi Monteiro <monteiro_davi@yahoo.com>
2024-02-15 09:29:22 -0300

    Criando decorator customizado para o usuário criar saves apenas para ele mesmo

--- src/save/save.controller.ts
+++ src/save/save.controller.ts
@@ -11,17 +11,18 @@ import { CreateSaveDto } from './dtos/createSave.dto';
 import { SaveEntity } from './entities/save.entity';
 import { Roles } from 'src/decorators/roles.decorator';
 import { UserType } from 'src/user/enums/type.enum';
+import { idUser } from 'src/decorators/userId.decorator';
 
 @Roles(UserType.User)
 @Controller('save')
 export class SaveController {
   constructor(private readonly saveService: SaveService) {}
 
-  @Post('/:idUser')
+  @Post()
   @UsePipes(ValidationPipe)
   async createSave(
     @Body() createSaveDto: CreateSaveDto,
-    @Param('idUser') idUser: number,
+    @idUser() idUser: number,
   ): Promise<SaveEntity> {
     return this.saveService.createSave(createSaveDto, idUser);
   }
