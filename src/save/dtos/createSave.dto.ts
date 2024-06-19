4c7abc4b38e34af5b4df90e0b984a8e0d44b2954
Davi Monteiro <monteiro_davi@yahoo.com>
2024-06-19 16:21:27 -0300

    Adicionando novas validações aos dtos de teamglobal e save

--- src/save/dtos/createSave.dto.ts
+++ src/save/dtos/createSave.dto.ts
@@ -1,6 +1,11 @@
-import { IsString } from 'class-validator';
+import { IsString, Length } from 'class-validator';
+import {
+  USER_MAX_LENGH_NAME,
+  USER_MIN_LENGH_NAME,
+} from 'src/utils/constants/dtoValidators';
 
 export class CreateSaveDTO {
   @IsString()
+  @Length(USER_MIN_LENGH_NAME, USER_MAX_LENGH_NAME)
   name: string;
 }
--- src/teamglobal/dtos/createTeamglobal.dto.ts
+++ src/teamglobal/dtos/createTeamglobal.dto.ts
@@ -1,17 +1,36 @@
-import { IsArray, IsNumber, IsString } from 'class-validator';
+import {
+  ArrayMaxSize,
+  ArrayMinSize,
+  IsArray,
+  IsInt,
+  IsString,
+  Length,
+  Validate,
+} from 'class-validator';
+import {
+  TEAMGLOBAL_MAX_LENGH_NAME,
+  TEAMGLOBAL_MAX_PLAYERSGLOBAL,
+  TEAMGLOBAL_MIN_LENGH_NAME,
+  TEAMGLOBAL_MIN_PLAYERSGLOBAL,
+} from 'src/utils/constants/dtoValidators';
+import { UniqueArray } from 'src/validators/customValidators';
 
 export class CreateTeamglobalDTO {
-  @IsNumber()
+  @IsInt()
   countryId: number;
 
-  @IsNumber()
+  @IsInt()
   managerglobalId: number;
 
   @IsArray()
-  @IsNumber({}, { each: true })
+  @IsInt({ each: true })
+  @Validate(UniqueArray)
+  @ArrayMinSize(TEAMGLOBAL_MIN_PLAYERSGLOBAL)
+  @ArrayMaxSize(TEAMGLOBAL_MAX_PLAYERSGLOBAL)
   playerglobalIds: number[];
 
   @IsString()
+  @Length(TEAMGLOBAL_MIN_LENGH_NAME, TEAMGLOBAL_MAX_LENGH_NAME)
   name: string;
 
   @IsString()
