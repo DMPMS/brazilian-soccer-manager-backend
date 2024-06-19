a2641febc32b785e8d2f0334423611ae44d2b451
Davi Monteiro <monteiro_davi@yahoo.com>
2024-06-19 13:42:40 -0300

    Adicionando novas validações aos dtos de managerglobal

--- src/managerglobal/dtos/createManagerGlobal.dto.ts
+++ src/managerglobal/dtos/createManagerGlobal.dto.ts
@@ -1,12 +1,21 @@
-import { IsNumber, IsString } from 'class-validator';
+import { IsInt, IsString, Length, Max, Min } from 'class-validator';
+import {
+  MANAGERGLOBAL_MAX_AGE,
+  MANAGERGLOBAL_MAX_LENGH_NAME,
+  MANAGERGLOBAL_MIN_AGE,
+  MANAGERGLOBAL_MIN_LENGH_NAME,
+} from 'src/utils/constants/dtoValidators';
 
 export class CreateManagerglobalDTO {
-  @IsNumber()
+  @IsInt()
   countryId: number;
 
   @IsString()
+  @Length(MANAGERGLOBAL_MIN_LENGH_NAME, MANAGERGLOBAL_MAX_LENGH_NAME)
   name: string;
 
-  @IsNumber()
+  @IsInt()
+  @Min(MANAGERGLOBAL_MIN_AGE)
+  @Max(MANAGERGLOBAL_MAX_AGE)
   age: number;
 }
--- src/utils/constants/dtoValidators.ts
+++ src/utils/constants/dtoValidators.ts
@@ -1,2 +1,7 @@
 export const USER_MIN_LENGH_NAME = 3;
 export const USER_MAX_LENGH_NAME = 40;
+
+export const MANAGERGLOBAL_MIN_LENGH_NAME = 3;
+export const MANAGERGLOBAL_MAX_LENGH_NAME = 40;
+export const MANAGERGLOBAL_MIN_AGE = 18;
+export const MANAGERGLOBAL_MAX_AGE = 90;
