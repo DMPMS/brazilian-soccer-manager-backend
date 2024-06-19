8f2214a1a95768394ce06fa5494cc93247db780c
Davi Monteiro <monteiro_davi@yahoo.com>
2024-06-19 13:27:24 -0300

    Adicionando novas validações aos dtos de user

--- src/user/dtos/createUser.dto.ts
+++ src/user/dtos/createUser.dto.ts
@@ -1,7 +1,12 @@
-import { IsString } from 'class-validator';
+import { IsString, Length } from 'class-validator';
+import {
+  USER_MAX_LENGH_NAME,
+  USER_MIN_LENGH_NAME,
+} from 'src/utils/constants/dtoValidators';
 
 export class CreateUserDTO {
   @IsString()
+  @Length(USER_MIN_LENGH_NAME, USER_MAX_LENGH_NAME)
   name: string;
 
   @IsString()
---
+++ src/utils/constants/dtoValidators.ts
@@ -0,0 +1,2 @@
+export const USER_MIN_LENGH_NAME = 3;
+export const USER_MAX_LENGH_NAME = 40;
